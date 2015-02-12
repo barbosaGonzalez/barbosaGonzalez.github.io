bdaf9f80cb064a0ad1fc58623a63cf6069179639
Edsel Barbosa Gonzalez <torby@outlook.com>
2015-02-12 12:18:48 -0600

    MVC
    Agregacion de modelo vista controlador

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php include(vistas/header.html) ?>
+                   	  <li class="current-page"><a href="index.html">Inicio</a></li>
+                      <li><a href="about.html">Acerca de</a></li>
+                      <li><a href="services.html">Proyectos</a></li>
+                      <li><a href="#">Libros</a> </li>	
+                      <li><a href="#">Programas</a></li>
+                      <li><a href="tesis.html">Tesis</a></li>
+                      <li><a href="contact.html">Contacto</a></li>
+            	 </ul>
+           	  </nav>
+         </div>
+        </div>
+       </div>
+      </div>
+			<div class="clear"> </div>
+			<div class="image-slider">
+				<!-- Slideshow 1 -->
+			    <ul class="rslides" id="slider1">
+			      <li><img src="images/slider-slide-1.jpg" alt=""></li>
+			      <li><img src="images/slider-slide-2.jpg" alt=""></li>
+			      <li><img src="images/slider-slide-3.png" alt=""></li>
+			    </ul>
+				 <!-- Slideshow 2 -->
+			</div>
+			<div class="wrap">
+			<div class="content">
+				<h1>Ultimos 3 proyectos universitarios</h1>
+				<div class="grid">
+					<span class="link-1">04<a href="#"> Junio, 2013</a></span>
+					<h4>Sistema de evaluación de bibliotecas.</h4>
+					<p>>Conoser la eficiencia del servicio brindado por las bibliotecas de la Universidad de Colima.</p>
+				</div>
+				
+				<div class="grid">
+					<span class="link-1">05<a href="#"> Diciembre, 2013</a></span>
+					<h4>Tarjeta inteligente de pago.</h4>
+					<p>Sistema de tarjeas de prepago para el transporte publico en zona conurbada Colima-Villa de Alvarez. </p>
+				</div>				
+				<div class="grid">
+					<span class="link-1">04<a href="#"> Junio, 2014</a></span>
+					<h4>Control de flujo de agua.</h4>
+					<p>Sistema para medir y controlar el agua utilizada en la Universidad de Colima.</p>
+				</div>
+				<div class="clear"> </div>
+			<div class="clear"> </div>
+			</div>
+			</div>
+<?php include(vistas/footer.html) ?>
